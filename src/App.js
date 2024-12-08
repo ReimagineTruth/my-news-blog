86a96f29ec4f9dbf201e044a1ad1808956278fe4
Reimagine Truth <thephantomelder@gmail.com>
2024-12-08 10:14:53 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddPost from './components/AddPost';
+import DisplayPosts from './components/DisplayPosts';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>My Blog</h1>
+      <AddPost />
+      <DisplayPosts />
+    </div>
+  );
+};
+
+export default App;
